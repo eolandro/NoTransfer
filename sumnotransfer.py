df3f08306ab812d8c68d358da65777bfdf624559
eolandro <eolandro@gmx.com>
2026-04-21 00:39:28 -0600

    mejora?

--- sumnotransfer.py
+++ sumnotransfer.py
@@ -10,7 +10,6 @@ def byte2sum(NA, N):
     if NA == N:
         return (N - 1, '+',1)
 
-#N = [ byte2sum(random.randint(0,255),a) for a in range(256) ]
 
 def main():
         parser = argparse.ArgumentParser(
@@ -20,10 +19,6 @@ def main():
         parser.add_argument(
                 'file2NoTransfer', type=argparse.FileType('rb'),
                 help='Origin File'
-        )
-        parser.add_argument(
-                'file_output', type=argparse.FileType('wb'),
-                help='Output File'
         )
 
         args = parser.parse_args()
@@ -35,11 +30,9 @@ def main():
                     continue
                 m = byte2sum(random.randint(0,255),bt[0])
                 m = [ str(s) for s in m]
-                m = ''.join(m).encode()
-                args.file_output.write(m)
-                args.file_output.write(b'\x0a')
+                m = ''.join(m)
+                print(m)
         args.file2NoTransfer.close()
-        args.file_output.close()
 
 
 if __name__ == '__main__':
